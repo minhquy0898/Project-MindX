69041c15c47d741873693b10bd0f55657168c7a1
minhquy0898 <minhquy43123@gmail.com>
2023-06-26 21:33:41 +0700

    Project

---
+++ z-DoAnMindX/js/product.js
@@ -0,0 +1,67 @@
+export const text = {
+    contact: [{
+        productTitle: "Nhẫn ngọc trai cao cấp",
+        productPrice: 998.000
+    },
+    {
+        productTitle: "Vòng tay cao cấp",
+        productPrice: 500.000
+    },
+    {
+        productTitle: "Nhẫn vàng cao cấp",
+        productPrice: 400.000
+    },
+    {
+        productTitle: "Nhẫn kim cương cao cấp",
+        productPrice: 700.000
+    },
+    {
+        productTitle: "Nhẫn tình yêu",
+        productPrice: 900.000
+    },
+    {
+        productTitle: "Bông tai cao cấp",
+        productPrice: 500.000
+    },
+    {
+        productTitle: "Bông tai ngọc trai",
+        productPrice: 600.000
+    },
+    {
+        productTitle: "Nhẫn đôi kim cương",
+        productPrice: 910.000
+    },
+    {
+        productTitle: "Chìa khóa tình yêu",
+        productPrice: 400.000
+    },
+    {
+        productTitle: "Dây chuyền số 8",
+        productPrice: 230.000
+    },
+    {
+        productTitle: "Vòng cổ đá xanh",
+        productPrice: 300.000
+    },
+    {
+        productTitle: "Nhẫn vòng basis",
+        productPrice: 400.000
+    },
+    {
+        productTitle: "Bông tai phá cách",
+        productPrice: 740.000
+    },
+    {
+        productTitle: "Nhẫn vòng ADV",
+        productPrice: 780.000
+    },
+    {
+        productTitle: "Vòng tay tình nhân",
+        productPrice: 690.000
+    },
+    {
+        productTitle: "Nhẫn vòng đặc biệt",
+        productPrice: 999.000
+    },
+    ]
+}
