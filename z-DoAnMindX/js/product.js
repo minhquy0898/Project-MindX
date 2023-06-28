9a2addde730a6cd27724021e2ebbebaaed738ab2
duy1111 <maixuanduy0605@gmail.com>
2023-06-28 09:31:39 +0700

    fixed #4

--- z-DoAnMindX/js/product.js
+++ z-DoAnMindX/js/product.js
@@ -1,83 +1,83 @@
 var text = {
     contact: [{
         productTitle: "Nhẫn ngọc trai cao cấp",
-        productPrice: 998.000,
+        productPrice: 998 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Vòng tay cao cấp",
-        productPrice: 500.000,
+        productPrice: 500 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn vàng cao cấp",
-        productPrice: 400.000,
+        productPrice: 400 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn kim cương cao cấp",
-        productPrice: 700.000,
+        productPrice: 700 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn tình yêu",
-        productPrice: 900.000,
+        productPrice: 900 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Bông tai cao cấp",
-        productPrice: 500.000,
+        productPrice: 500 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Bông tai ngọc trai",
-        productPrice: 600.000,
+        productPrice: 600 ,
         image: './img/nhan-doi-kim-cuong.webp'
 
     },
     {
         productTitle: "Nhẫn đôi kim cương",
-        productPrice: 910.000,
+        productPrice: 910 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Chìa khóa tình yêu",
-        productPrice: 400.000,
+        productPrice: 400 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Dây chuyền số 8",
-        productPrice: 230.000,
+        productPrice: 230 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Vòng cổ đá xanh",
-        productPrice: 300.000,
+        productPrice: 300 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn vòng basis",
-        productPrice: 400.000,
+        productPrice: 400 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Bông tai phá cách",
-        productPrice: 740.000,
+        productPrice: 740 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn vòng ADV",
-        productPrice: 780.000,
+        productPrice: 780 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Vòng tay tình nhân",
-        productPrice: 690.000,
+        productPrice: 690 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     {
         productTitle: "Nhẫn vòng đặc biệt",
-        productPrice: 999.000,
+        productPrice: 999 ,
         image: './img/nhan-doi-kim-cuong.webp'
     },
     ]
